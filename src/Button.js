5d7b6d55fd90cbbcfa50b52791ca51eb5cd57d83
leedo97y <dlehdus1433@gmail.com>
2022-08-09 14:48:12 +0900

    how to handle CSS in separate component

--- src/Button.js
+++ src/Button.js
@@ -4,6 +4,7 @@ import styles from "./Button.module.css";
 
 function Button({ text }) {
   return <button className={styles.btn}>{text}</button>;
+  // React make random className for this component
 }
 
 Button.propTypes = {
