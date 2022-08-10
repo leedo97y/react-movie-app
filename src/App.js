748044b283cf2125351d4b263fb05e8dbf3c29ce
leedo97y <dlehdus1433@gmail.com>
2022-08-10 22:59:06 +0900

    learn how to use useEffect in React

--- src/App.js
+++ src/App.js
@@ -7,17 +7,24 @@ function App() {
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
 
-  console.log("i run all the time..");
   useEffect(() => {
-    console.log("call the API ..");
+    console.log("i run only once.");
   }, []);
   // 여기서는 리액트가 따로 지켜볼 코드가 없기때문에 한번만 실행된다.
   useEffect(() => {
-    console.log("Search for", keyword);
+    console.log("i run when 'keyword' changes.");
   }, [keyword]);
-  // if keyword change, console code execute
+  // if keyword changes, console code executed
   // 특정 부분이 변화할때 이 코드를 실행하고 싶다면,
   // {} 부분에 실행시킬 코드를 적고, 변화하는 부분을 []안에 넣어주면 된다.
+
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("i run when 'keyword & counter' changes.");
+  }, [keyword, counter]);
 
   return (
     <div>
