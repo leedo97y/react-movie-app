d2c25c55595963982c7b7a5355248b5795b8588e
leedo97y <dlehdus1433@gmail.com>
2022-08-12 16:57:43 +0900

    add to-Do Prac on Prac folder

---
+++ pracFile/toDoPrac.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function ToDoList() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+      // kill the function
+    } else {
+      setToDos((currentArray) => [toDo, ...currentArray]); // send function
+      // first argument : currentArray
+      // second argument : [toDo, ...currentArray]
+      // -> by using 'Spread' 연산자
+      setToDo(""); // empty input value
+    }
+  };
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do ... "
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created!");
-    return () => {
-      console.log("destroyed!");
-    };
-  }, []);
-  return <h1>Hello</h1>;
-}
-// CleanUp function 이라고 하며,
-// useEffect에서 부분적으로 반복될 코드를 작성한 뒤, return 뒤에 함수를 작성해서 만들어준다.
-
-// 위의 경우는 hello 함수의 showing state가 true이면 created!라는 텍스트를 출력해주며,
-// hello 함수의 showing state가 false이면 destroyed!라는 텍스트를 출력해준다.
-// 크게 많이 쓰이는 편은 아니지만, 실행방식을 이해하는 것이 중요함.
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => !prev);
-  };
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
