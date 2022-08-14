8f3cf596c677abaac21f913a6f490015632dab08
leedo97y <dlehdus1433@gmail.com>
2022-08-15 00:29:08 +0900

    add Router path, add comment for router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/Movie">
+          <Detail />
+        </Route>
         <Route path="/">
           <Home />
         </Route>
@@ -14,3 +18,6 @@ function App() {
 }
 
 export default App;
+
+// Hash Router는 / 전에 #가 붙는다.
+// Browser Router를 일반적으로 쓴다.
