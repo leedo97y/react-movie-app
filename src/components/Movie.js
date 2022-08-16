1c1e813983e6fe6f387dd40bc36ddce46adfd369
leedo97y <dlehdus1433@gmail.com>
2022-08-16 21:41:22 +0900

    import Movie css file

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
 function Movie({ id, coverImage, title, year, summary, genres }) {
   return (
