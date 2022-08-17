852b45fce0dd4fc183d6222da56479221bd3aa2e
leedo97y <dlehdus1433@gmail.com>
2022-08-17 21:51:17 +0900

    movie main page design

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,24 +2,27 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styles from "./Movie.module.css";
 
-function Movie({ id, coverImage, title, year, summary, genres }) {
+function Movie({ id, rating, coverImage, title, year, summary, genres }) {
   return (
-    <div className={styles.body}>
-      <img className={styles.img} src={coverImage} alt={title} />
-      <h2 className={styles.h2}>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <span className={styles.year}>{year}</span>
-      <p className={styles.summary}>
-        {summary.length > 235 ? `${summary.slice(0, 235)} ...` : summary}
-      </p>
-      <ul className={styles.allGenres}>
-        {genres.map((g) => (
-          <li className={styles.genre} key={g}>
-            {g}
-          </li>
-        ))}
-      </ul>
+    <div className={styles.all}>
+      <div className={styles.body}>
+        <img className={styles.img} src={coverImage} alt={title} />
+        <span className={styles.rate}>★ {rating}</span>
+        <h2 className={styles.h2}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <span className={styles.year}>{year}</span>
+        <ul className={styles.allGenres}>
+          {genres.map((g) => (
+            <li className={styles.genre} key={g}>
+              {g}
+            </li>
+          ))}
+        </ul>
+        <p className={styles.summary}>
+          {summary.length > 235 ? `${summary.slice(0, 235)} ...` : summary}
+        </p>
+      </div>
     </div>
   );
 }
