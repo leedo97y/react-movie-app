d515af5bc88537bf370963ccc058415a32f2a138
leedo97y <dlehdus1433@gmail.com>
2022-08-17 21:50:52 +0900

    detail page design

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -5,6 +5,7 @@ import styles from "./Detail.module.css";
 function Detail() {
   const [movieInfo, setMovieInfo] = useState([]);
   const [genre, setGenre] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const { id } = useParams();
 
@@ -14,38 +15,46 @@ function Detail() {
     ).json();
     setMovieInfo(json.data.movie);
     setGenre(json.data.movie.genres);
+    setLoading(false);
     console.log(json);
   };
+
   useEffect(() => {
     getMovie();
   }, []);
-  return (
-    <div className={styles.body}>
-      <img
-        className={styles.img}
-        src={movieInfo.large_cover_image}
-        alt={movieInfo.title}
-      ></img>
-      <h2 className={styles.title}>{movieInfo.title}</h2>
-      <div className={styles.yearBody}>
-        <span className={styles.year}> {movieInfo.year}</span>
-        <span className={styles.star}>rate {movieInfo.rating}</span>
-      </div>
-      <div className={styles.likeBody}>
-        <span className={styles.like}> Like {movieInfo.like_count}</span>
-        <span className={styles.runtime}>
-          {movieInfo.runtime !== 0 ? `runtime ${movieInfo.runtime}m` : null}
-        </span>
-      </div>
 
-      <ul className={styles.allGenres}>
-        {genre.map((g) => (
-          <li className={styles.genre} key={g}>
-            {g}
-          </li>
-        ))}
-      </ul>
-      <p className={styles.desc}>{movieInfo.description_full}</p>
+  return (
+    <div>
+      {loading ? (
+        <h1 className={styles.loading}>Loading...</h1>
+      ) : (
+        <div className={styles.allBody}>
+          <img
+            className={styles.img}
+            src={movieInfo.large_cover_image}
+            alt={movieInfo.title}
+          ></img>
+          <h2 className={styles.title}>{movieInfo.title}</h2>
+          <div className={styles.yearBody}>
+            <span className={styles.year}> {movieInfo.year}</span>
+            <span className={styles.star}>rate {movieInfo.rating}</span>
+          </div>
+          <div className={styles.likeBody}>
+            <span className={styles.like}> Like {movieInfo.like_count}</span>
+            <span className={styles.runtime}>
+              {movieInfo.runtime !== 0 ? `runtime ${movieInfo.runtime}m` : null}
+            </span>
+          </div>
+          <ul className={styles.allGenres}>
+            {genre.map((g) => (
+              <li className={styles.genre} key={g}>
+                {g}
+              </li>
+            ))}
+          </ul>
+          <p className={styles.desc}>{movieInfo.description_full}</p>
+        </div>
+      )}
     </div>
   );
 }
